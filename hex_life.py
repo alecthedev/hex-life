42dbf61680bc2463d64fa76e166c00c3075bdca1
alecthedev <alecthedeveloper@gmail.com>
2024-07-30 17:15:43 -0400

    update gui

--- hex_life.py
+++ hex_life.py
@@ -4,53 +4,60 @@ from tkinter import Canvas, Tk, ttk
 from hex import HexManager
 from vector2 import Vector2
 
+print("Launching Simulation...")
+
+
 WIDTH, HEIGHT = 1200, 850
 
-
-def build_sim():
-    root = Tk()
-    root.title("Hex Life - github.com/alecthedev")
-    content = ttk.Frame(root)
-    controls = ttk.Frame(root)
-
-    canvas = Canvas(content, width=WIDTH, height=HEIGHT, bg="black")
-    hex_manager = HexManager(Vector2(WIDTH // 2, HEIGHT // 2), 4, 70, canvas, seed=None)
-    current_gen = hex_manager.generation
-
-    generation_label = ttk.Label(content, text=f"Generation: {current_gen}")
-
-    if current_gen != hex_manager.generation:
-        current_gen = hex_manager.generation
-        generation_label.setvar("text")
-
-    play_pause_button = ttk.Button(
-        controls, text="Play/Pause", command=hex_manager.toggle_running
-    )
-    reset_button = ttk.Button(
-        controls, text="Reset World", command=hex_manager.reset_world
-    )
-    randomize_button = ttk.Button(
-        controls, text="Randomize World", command=hex_manager.seed_world
-    )
-
-    content.grid(column=0, row=0, columnspan=2)
-    canvas.grid(column=0, row=0)
-    generation_label.grid(column=0, row=1)
-
-    controls.grid(column=3, row=0, columnspan=1)
-    play_pause_button.grid(column=0, row=0)
-    reset_button.grid(column=0, row=1)
-    randomize_button.grid(column=0, row=2)
-
-    num = randint(0, 999)
-    print(num)
-
-    root.bind("<Return>", hex_manager.update_world)
-
-    root.mainloop()
+# seed generation
+# TODO: update seed generation to larger range, store/display for user?
+num = randint(0, 999)
+print(f"seed: {num}")
 
 
-if __name__ == "__main__":
-    print("Launching Simulation...")
-    build_sim()
-    print("Exiting...")
+# create window and frames
+root = Tk()
+root.title("Hex Life - github.com/alecthedev")
+content = ttk.Frame(root)
+controls = ttk.Frame(root)
+
+# create canvas for drawing, create simulation manager
+canvas = Canvas(content, width=WIDTH, height=HEIGHT, bg="black")
+hex_manager = HexManager(Vector2(WIDTH // 2, HEIGHT // 2), 4, 70, canvas, seed=None)
+
+# create labels and buttons
+generation_label = ttk.Label(content, text=f"Generation: {hex_manager.generation}")
+
+play_pause_button = ttk.Button(
+    controls, text="Play/Pause", command=hex_manager.toggle_running
+)
+reset_button = ttk.Button(controls, text="Reset World", command=hex_manager.reset_world)
+randomize_button = ttk.Button(
+    controls, text="Randomize World", command=hex_manager.seed_world
+)
+
+# add content items to grid
+content.grid(column=0, row=0, columnspan=2)
+canvas.grid(column=0, row=0)
+generation_label.grid(column=0, row=1)
+
+# add control items to grid
+controls.grid(column=3, row=0, columnspan=1)
+play_pause_button.grid(column=0, row=0)
+reset_button.grid(column=0, row=1)
+randomize_button.grid(column=0, row=2)
+
+root.bind("<Return>", hex_manager.update_world)
+
+
+def update_labels():
+    generation_label.config(text=f"Generation: {hex_manager.generation}")
+    root.after(5, update_labels)
+
+
+update_labels()
+
+root.mainloop()
+
+
+print("Exiting...")
