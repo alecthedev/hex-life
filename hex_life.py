f21a8bcfa50ebf6752c0f1a5635a80f23d41bfc3
alecthedev <alecthedeveloper@gmail.com>
2024-08-07 16:33:45 -0400

    add incremental labels

--- hex_life.py
+++ hex_life.py
@@ -23,11 +23,16 @@ canvas = Canvas(content, width=WIDTH, height=HEIGHT, bg="black")
 
 hex_manager = HexManager(Vector2(WIDTH // 2, HEIGHT // 2), canvas)
 
-generation_label = ttk.Label(root, text=f"Generation: {hex_manager.generation}")
+generation_label = ttk.Label(content, text=f"Generation: {hex_manager.generation}")
 instructions = ttk.Label(
     options,
     text="""
     Welcome to Hex Life!
+
+
+
+
+
 
 
     Left-click to Add Cell
@@ -50,6 +55,8 @@ speed_dec_button = ttk.Button(
     controls,
     text="Speed −",
 )
+speed_label_text = "Speed: █ █ _ _"
+speed_label = ttk.Label(controls, text=speed_label_text)
 
 # settings frame buttons and labels
 settings_label = ttk.Label(settings, text="Settings", padding=(0, 20))
@@ -63,14 +70,16 @@ size_dec_button = ttk.Button(
 size_inc_button = ttk.Button(
     settings, text="World Size +", command=hex_manager.increase_world_size
 )
+size_label_text = "World Size: █ █ _ _"
+size_label = ttk.Label(settings, text=size_label_text)
 
 # root grid
 content.grid(column=0, row=1, columnspan=2)
 options.grid(column=2, row=1, columnspan=1, padx=15)
-generation_label.grid(column=0, row=2, columnspan=2)
 
 # content frame grid
 canvas.grid(column=0, row=0, padx=15)
+generation_label.grid(column=0, row=1, pady=30)
 
 # option frame grid
 instructions.grid(column=0, row=0)
@@ -81,19 +90,46 @@ settings.grid(column=0, row=5)
 controls_label.grid(column=0, row=0, columnspan=2)
 play_pause_button.grid(column=0, row=1)
 step_button.grid(column=1, row=1)
-speed_dec_button.grid(column=0, row=2)
-speed_inc_button.grid(column=1, row=2)
+speed_dec_button.grid(column=0, row=2, padx=5, pady=5)
+speed_inc_button.grid(column=1, row=2, padx=5, pady=5)
+speed_label.grid(column=0, row=3, columnspan=2, pady=15)
 
 # settings frame grid
 settings_label.grid(column=0, row=0, columnspan=2)
 reset_button.grid(column=0, row=1)
 randomize_button.grid(column=1, row=1)
-size_dec_button.grid(column=0, row=2)
-size_inc_button.grid(column=1, row=2)
+size_dec_button.grid(column=0, row=2, padx=5, pady=5)
+size_inc_button.grid(column=1, row=2, padx=5, pady=5)
+size_label.grid(column=0, row=3, columnspan=2, pady=15)
+
+
+def update_size_label():
+    label = "Size: "
+    if hex_manager.curr_size == hex_manager.world_sizes["small"]:
+        label += "█ _ _ _"
+    elif hex_manager.curr_size == hex_manager.world_sizes["medium"]:
+        label += "█ █ _ _"
+    elif hex_manager.curr_size == hex_manager.world_sizes["large"]:
+        label += "█ █ █ _"
+    else:
+        label += "█ █ █ █"
+    return label
+
+
+def update_speed_label():
+    return ""
 
 
 def update_labels():
     generation_label.config(text=f"Generation: {hex_manager.generation}")
+
+    speed_label_text = update_speed_label()
+    size_label_text = update_size_label()
+
+    speed_label.config(text=speed_label_text)
+
+    size_label.config(text=size_label_text)
+
     root.after(5, update_labels)
 
 
