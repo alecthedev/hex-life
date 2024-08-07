8935dc0c0c830fbeaaebebffcbb071a11031893c
alecthedev <alecthedeveloper@gmail.com>
2024-08-07 14:31:31 -0400

    update gui

--- hex_life.py
+++ hex_life.py
@@ -1,4 +1,3 @@
-from random import randint
 from tkinter import Canvas, Tk, ttk
 
 from hex import HexManager
@@ -9,37 +8,91 @@ print("Launching Simulation...")
 
 WIDTH, HEIGHT = 1000, 875
 
+world_sizes = {
+    "small": (15, 18),
+    "medium": (10, 28),
+    "large": (7, 40),
+    "huge": (4, 70),
+}
+
+curr_world_size = world_sizes["medium"]
+
 # create window and frames
 root = Tk()
 root.title("Hex Life - github.com/alecthedev")
+root.resizable(False, False)
 content = ttk.Frame(root)
-controls = ttk.Frame(root)
+options = ttk.Frame(root)
 
-# create canvas for drawing, create simulation manager
+controls = ttk.Frame(options, padding=(0, 100))
+settings = ttk.Frame(options, padding=(0, 100))
+
+# canvas for drawing and simulation manager
 canvas = Canvas(content, width=WIDTH, height=HEIGHT, bg="black")
-hex_manager = HexManager(Vector2(WIDTH // 2, HEIGHT // 2), 10, 28, canvas)
 
-# create labels and buttons
-generation_label = ttk.Label(content, text=f"Generation: {hex_manager.generation}")
+hex_manager = HexManager(
+    Vector2(WIDTH // 2, HEIGHT // 2), curr_world_size[0], curr_world_size[1], canvas
+)
 
+generation_label = ttk.Label(root, text=f"Generation: {hex_manager.generation}")
+instructions = ttk.Label(
+    options,
+    text="""
+    Welcome to Hex Life!
+
+
+    Left-click to Add Cell
+
+    Right-click to Remove
+""",
+)
+
+# control frame buttons and labels
+controls_label = ttk.Label(controls, text="Controls", padding=(0, 20))
 play_pause_button = ttk.Button(
     controls, text="Play/Pause", command=hex_manager.toggle_running
 )
-reset_button = ttk.Button(controls, text="Reset World", command=hex_manager.reset_world)
-randomize_button = ttk.Button(
-    controls, text="Randomize World", command=hex_manager.seed_world
+step_button = ttk.Button(controls, text="Next Gen", command=hex_manager.update_world)
+speed_inc_button = ttk.Button(
+    controls,
+    text="Speed +",
+)
+speed_dec_button = ttk.Button(
+    controls,
+    text="Speed −",
 )
 
-# add content items to grid
-content.grid(column=0, row=0, columnspan=2)
-canvas.grid(column=0, row=0)
-generation_label.grid(column=0, row=1)
+# settings frame buttons and labels
+settings_label = ttk.Label(settings, text="Settings", padding=(0, 20))
+reset_button = ttk.Button(settings, text="Reset World", command=hex_manager.reset_world)
+randomize_button = ttk.Button(
+    settings, text="Randomize", command=hex_manager.seed_world
+)
 
-# add control items to grid
-controls.grid(column=3, row=0, columnspan=1)
-play_pause_button.grid(column=0, row=0)
+# root grid
+content.grid(column=0, row=1, columnspan=2)
+options.grid(column=2, row=1, columnspan=1, padx=15)
+generation_label.grid(column=0, row=2, columnspan=2)
+
+# content frame grid
+canvas.grid(column=0, row=0, padx=15)
+
+# option frame grid
+instructions.grid(column=0, row=0)
+controls.grid(column=0, row=3)
+settings.grid(column=0, row=5)
+
+# control frame grid
+controls_label.grid(column=0, row=0, columnspan=2)
+play_pause_button.grid(column=0, row=1)
+step_button.grid(column=1, row=1)
+speed_dec_button.grid(column=0, row=2)
+speed_inc_button.grid(column=1, row=2)
+
+# settings frame grid
+settings_label.grid(column=0, row=0, columnspan=2)
 reset_button.grid(column=0, row=1)
-randomize_button.grid(column=0, row=2)
+randomize_button.grid(column=1, row=1)
 
 root.bind("<Return>", hex_manager.update_world)
 
